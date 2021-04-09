0fd05348de1a4b05845ef3838aa40f72b94a7817
bluk <bluk16@users.noreply.github.com>
2021-04-09 09:58:48 -0400

    fix walkscore

--- scraper.py
+++ scraper.py
@@ -9,6 +9,7 @@ import pandas as pd
 import numpy as np
 import niche_scrape as niche
 import mls_scrape as mls
+import walkscore as walkscore
 
 web_driver = webdriver.Chrome('/usr/local/bin/chromedriver')
 
@@ -16,3 +17,7 @@ df = mls.get_listings(web_driver)
 print(df)
 neighborhood = niche.get_niche_grade(web_driver,1)
 print(neighborhood)
+walkscore, transitscore, bikescore = walkscore.get_scores(df.iloc[0,0])
+print(walkscore)
+print(transitscore)
+print(bikescore)
--- walkscore.py
+++ walkscore.py
@@ -1,10 +1,20 @@
 from geopy import Nominatim
+from geopy import MapQuest
 import json
 import urllib
 
-def walkscore(location_str):
+MAPQUEST_API_KEY = 'M4GKnzAc6Pyz418vyJyXlmSu4PqSsOFR'
+
+def get_location(location_str):
     locator = Nominatim(user_agent='myGeocoder')
     location = locator.geocode(location_str)
+    if not location:
+        locator = MapQuest(MAPQUEST_API_KEY)
+        location = locator.geocode(location_str)
+    return location
+
+def walkscore(location_str):
+    location = get_location(location_str)
     website = 'https://api.walkscore.com/score?format=json&lat=' + str(location.latitude) + '&lon=' + str(location.longitude) +'&transit=1&bike=1&wsapikey=b1c9cb0cbfab72947704a84addb4a9e9'
     with urllib.request.urlopen(website) as url:
         data = json.loads(url.read().decode())
@@ -12,5 +22,13 @@ def walkscore(location_str):
     print('transit score is ' + str(data['transit']['score']) + '\n')
     print('bike score is ' + str(data['bike']['score']) + '\n')
 
-var  = input('Enter Address:')
+def get_scores(location_str):
+    location = get_location(location_str)
+    website = 'https://api.walkscore.com/score?format=json&lat=' + str(location.latitude) + '&lon=' + str(location.longitude) +'&transit=1&bike=1&wsapikey=b1c9cb0cbfab72947704a84addb4a9e9'
+    with urllib.request.urlopen(website) as url:
+        data = json.loads(url.read().decode())
+
+    return str(data['walkscore']), str(data['transit']['score']), str(data['bike']['score'])
+
+var  = input('Enter Address: ')
 walkscore(var)
